5d7d69bd2f017943c8e20c7b3347166d3f1cd76b
AhnEunJin <modic970515@gmail.com>
2024-08-18 21:22:23 +0900

    현재기반 위치 가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,17 @@
+import { useEffect } from 'react';
 import './App.css';
 
 function App() {
+  useEffect(() => {
+    getCurrentLocation();
+  }, []);
+
+  const getCurrentLocation = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      const latitude = position.coords.latitude;
+      const longitude = position.coords.longitude;
+    });
+  };
   return <div></div>;
 }
 
