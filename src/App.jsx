df8e091d48ec17f2fa3fdd8b5ae78faafe2370a5
AhnEunJin <modic970515@gmail.com>
2024-08-19 00:47:03 +0900

    loading spinner

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,14 @@ import './App.css';
 import axios from 'axios';
 import WeatherBox from './components/WeatherBox';
 import WeatherButton from './components/WeatherButton';
+import ClipLoader from 'react-spinners/ClipLoader';
 
 function App() {
   const cities = ['new york', 'paris', 'tokyo'];
 
   const [weather, setWeather] = useState(null);
   const [city, setCity] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     if (city === '') {
@@ -28,22 +30,37 @@ function App() {
   };
 
   const getWeatherByCurrentLocation = async (lat, lon) => {
+    setLoading(true);
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=953f0c6e931e61136763636928559bf1&units=metric`;
     const res = await axios.get(url);
     setWeather(res.data);
+    setLoading(false);
   };
 
   const getWeatherByCity = async () => {
+    setLoading(true);
     const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=953f0c6e931e61136763636928559bf1&units=metric`;
     const res = await axios.get(url);
     setWeather(res.data);
+    setLoading(false);
   };
 
   return (
     <div>
       <div className='container'>
-        <WeatherBox weather={weather} />
-        <WeatherButton cities={cities} setCity={setCity} />
+        {loading ? (
+          <ClipLoader
+            color={'white'}
+            loading={loading}
+            size={100}
+            aria-label='Loading Spinner'
+          />
+        ) : (
+          <>
+            <WeatherBox weather={weather} />
+            <WeatherButton cities={cities} setCity={setCity} />
+          </>
+        )}
       </div>
     </div>
   );
