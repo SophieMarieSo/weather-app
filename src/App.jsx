ba10390c2f870570946bfe3f673bbf9f8d26a270
AhnEunJin <modic970515@gmail.com>
2024-08-18 21:33:28 +0900

    현재위치 기반 날씨 가져오기

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
 function App() {
   useEffect(() => {
@@ -10,8 +11,20 @@ function App() {
     navigator.geolocation.getCurrentPosition((position) => {
       const latitude = position.coords.latitude;
       const longitude = position.coords.longitude;
+
+      getWeatherByCurrentLocation(latitude, longitude);
     });
   };
+
+  const getWeatherByCurrentLocation = async (lat, lon) => {
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=953f0c6e931e61136763636928559bf1`;
+    const res = await axios.get(url);
+
+    console.log('====================================');
+    console.log(res.data);
+    console.log('====================================');
+  };
+
   return <div></div>;
 }
 
